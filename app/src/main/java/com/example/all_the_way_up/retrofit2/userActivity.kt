45f0ed032e0712d6f8e47b9cc859638ff67d28dd
FarajAg <farajalqdwry@gmail.com>
2018-06-15 18:29:06 +0800

    secon recycler View Fixed

--- app/src/main/java/com/example/all_the_way_up/retrofit2/userActivity.kt
+++ app/src/main/java/com/example/all_the_way_up/retrofit2/userActivity.kt
@@ -3,14 +3,21 @@ package com.example.all_the_way_up.retrofit2
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.Adapter
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.transition.Transition
 import com.example.all_the_way_up.retrofit2.Retrofit.callingApiInterface
 import com.example.all_the_way_up.retrofit2.Retrofit.retrofiClinte
+import com.example.all_the_way_up.retrofit2.data.repos
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_user.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class userActivity : AppCompatActivity() {
 
@@ -33,15 +40,27 @@ class userActivity : AppCompatActivity() {
         val retrofit = retrofiClinte.instances
         jsonApi = retrofit.create(callingApiInterface::class.java)
 
+        //recyclerView
         repossRecyclerView.setHasFixedSize(true)
         repossRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        jsonApi.listRepos(username).enqueue(object : Callback<List<repos>> {
+            override fun onResponse(call: Call<List<repos>>?, response: Response<List<repos>>?) {
+                var listofrepos = response?.body()!!
+                repossRecyclerView.adapter = reposAdapter(response?.body()!!, this@userActivity)
+
+            }
+
+            override fun onFailure(call: Call<List<repos>>?, t: Throwable?) {
+            }
+        })
 
         fetchData(username)
 
 
     }
 
-    private fun fetchData(name : String) {
+    private fun fetchData(name: String) {
 
 //        compositeDisposable.add(jsonApi.listRepos(name)
 //                .observeOn(AndroidSchedulers.mainThread())
@@ -54,6 +73,6 @@ class userActivity : AppCompatActivity() {
 //                .subscribeOn(Schedulers.io())
 //                .observeOn(AndroidSchedulers.mainThread())
 //                .subscribe({users->displayData(users)})
-        )
+//        )
     }
 }
